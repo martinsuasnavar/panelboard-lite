454ca3ee820368aa11d178f93831248593945d2f
martinsuasnavar <145569146+martinsuasnavar@users.noreply.github.com>
2024-05-22 08:20:20 -0300

    Update GenericButton.jsx

--- client/src/buttons/GenericButton.jsx
+++ client/src/buttons/GenericButton.jsx
@@ -1,1 +1,14 @@
+const GenericButton = ({onClick, children, height, width}) =>{
+    const divStyle={
+        height: `${height}px`,
+        width: `${width}px`
+    };
 
+    return(
+        <a className="edit-button" onClick={onClick} style={divStyle}>
+            <span>{children}</span>
+        </a>
+    );
+};
+
+export default GenericButton;
